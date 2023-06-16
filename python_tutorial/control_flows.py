72d6bb5284260e4228738b28c3ceecf836399a35
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2023-06-16 16:55:04 +0200

    Add for statement and break clause in a for loop

--- python_tutorial/control_flows.py
+++ python_tutorial/control_flows.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def allow_driver(age: int) -> bool:
     return True if age > 18 else False
 
@@ -11,5 +14,31 @@ def is_allowed_driver(age: int) -> bool:
         return True
 
 
+def multiply_by_2(numbers: list[int, ...]) -> list[int, ...]:
+    return [num * 2 for num in numbers]
+
+
+def multiply_by_3(numbers: list[int, ...]) -> list[int, ...]:
+    res: list[int, ...] = []
+    for num in numbers:
+        res.append(num * 3)
+    return res
+
+
+def is_present(lista: list[Any, ...], e: Any) -> bool:
+    return any([element for element in lista if e == element])
+
+
+def is_present(lista: list[Any, ...], e: Any) -> bool:
+    for element in lista:
+        if e == element:
+            return True
+        break
+    return False
+
+
 if __name__ == '__main__':
-    allow_driver(23)
+    print(allow_driver(23))
+    print(multiply_by_2([1, 2, 3, 4, 5]))
+    print(multiply_by_3([1, 2, 3, 4, 5]))
+    print(is_present([1, 2, 3, 4, 5], 5))
